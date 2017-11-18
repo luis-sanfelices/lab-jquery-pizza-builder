323eeab791666c128a7596f840f71077b0d63364
luis-sanfelices <l.fernando.sanfelices@gmail.com>
2017-11-18 13:43:02 +0100

    first iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,18 @@
 // Write your Pizza Builder JavaScript in this file.
+$(document).ready(function() {
+    $(".btn-pepperonni").click(function(){
+        $(".pep").toggleClass('hidden');
+    });
+    $(".btn-mushrooms").click(function(){
+        $(".mushroom").toggleClass('hidden');
+    });
+    $(".btn-green-peppers").click(function(){
+        $(".green-pepper").toggleClass('hidden');
+    });
+    $(".btn-sauce").click(function(){
+        $(".sauce").toggleClass('hidden');
+    });
+    $(".btn-crust").click(function(){
+        $(".crust").toggleClass('hidden');
+    });
+});
